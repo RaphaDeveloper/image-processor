bb730b23bbf62b0207104ff8fbb62d47c19b5194
Raphael Silva <raphael.silva.xc@gmail.com>
2024-05-19 12:34:22 -0300

    Implements a hello world app with terraform configuring an ec2 instance with load balancer

---
+++ tf/main.tf
@@ -0,0 +1,184 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "elb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "elb-sg"
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-sg"
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "app_instance" {
+  ami             = "ami-0bb84b8ffd87024d8"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.deployer.key_name
+  subnet_id       = aws_subnet.public.id
+  security_groups = [aws_security_group.app_sg.name]
+
+  tags = {
+    Name = "AppInstance"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y git",
+      "curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -",
+      "sudo yum install -y nodejs",
+      "git clone https://github.com/RaphaDeveloper/image-processor.git",
+      "cd image-processor",
+      "npm install",
+      "sudo npm install -g pm2",
+      "pm2 start app.js --name node-app",
+      "pm2 startup systemd",
+      "pm2 save",
+      "sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u ec2-user --hp /home/ec2-user"
+    ]
+  }
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb_sg.id]
+  subnets            = [aws_subnet.public.id]
+
+  tags = {
+    Name = "app-lb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name = "app-tg"
+  port = 3000
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    interval = 30
+    timeout = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "app-tg"
+  }
+}
+
+resource "aws_lb_listener" "app_lb_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+
+  tags = {
+    Name = "app-lb-listener"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "app_tg_attachment" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id = aws_instance.app_instance.id
+  port = 3000
+}
